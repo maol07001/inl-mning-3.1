bca5337b2d790a390f218ede7ae540a15bd5f73c
maol07001 <maol07001@E-PW05CGR4>
2024-11-07 15:43:15 +0100

    Add project files.

---
+++ inlämning 3.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder=int.Parse(Console.ReadLine());
+            if (ålder == 16)
+            {
+                Console.WriteLine("du får gå på gymnasiet");
+            }
+            else if (ålder == 19)
+            {
+                Console.WriteLine("du får gå på gymnasiet");
+            }
+            else if (ålder == 17)
+            {
+                Console.WriteLine("du får gå på gymnasiet");
+            }
+            else if (ålder == 18)
+            {
+                Console.WriteLine("du får gå på gymnasiet");
+            }
+            else if (ålder > 20)
+            {
+                Console.WriteLine("Du får inte gå på gymnasiet");
+            }
+            else if (ålder < 15)
+            {
+                Console.WriteLine("Du får inte gå på gymnasiet");
+            }
+            else if(ålder ==15)
+            {
+                Console.WriteLine("Du får inte gå på gymnasiet");
+            }
+            else if (ålder ==20)
+            {
+                Console.WriteLine("Du får inte gå på gymnasiet");
+            }
+        }
+    }
+}
